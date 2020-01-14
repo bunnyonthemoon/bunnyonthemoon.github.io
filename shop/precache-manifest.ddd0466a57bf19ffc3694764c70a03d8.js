9a163756411bdde24302933a087e08d55215935f
bunnyonthemoon <greenbunnyonthemoon@outlook.com>
2020-01-14 03:40:22 +0300

    add shop

---
+++ shop/precache-manifest.ddd0466a57bf19ffc3694764c70a03d8.js
@@ -0,0 +1,566 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b734297ea6ea7a2b4004",
+    "url": "/css/chunk-0bfdaa3a.fcba48be.css"
+  },
+  {
+    "revision": "dd41b87bb3b4df8587af",
+    "url": "/css/chunk-4475bce5.60f1d2a4.css"
+  },
+  {
+    "revision": "283c202e35b538cafd76",
+    "url": "/css/chunk-554e2c80.c7dc6a9a.css"
+  },
+  {
+    "revision": "6bd72c89ea4ebdd783e3",
+    "url": "/css/chunk-6ce6342e.d9539349.css"
+  },
+  {
+    "revision": "6acd1593501ab07edd92",
+    "url": "/css/chunk-94988738.d5b53c34.css"
+  },
+  {
+    "revision": "916b9b4b3b867bf8d478",
+    "url": "/css/chunk-b929a942.ef8c8172.css"
+  },
+  {
+    "revision": "3947a7a60121259f7abb",
+    "url": "/css/chunk-fad08be4.35d0d7ad.css"
+  },
+  {
+    "revision": "f6244f47eb080989e9a5",
+    "url": "/css/chunk-fd2a7294.4ab3ff60.css"
+  },
+  {
+    "revision": "863eea74da0c0c81e520",
+    "url": "/css/chunk-vendors.bd3f40ae.css"
+  },
+  {
+    "revision": "965c234c49e7493d928b",
+    "url": "/css/index.5f770264.css"
+  },
+  {
+    "revision": "fd22baf51cbcd60e7017efbaa9a144ba",
+    "url": "/data/auth.json"
+  },
+  {
+    "revision": "ea53a5cd722546585027a0c2986ab329",
+    "url": "/data/categories.list.json"
+  },
+  {
+    "revision": "28e4897ee5f019dab00125d75b7ea5d5",
+    "url": "/data/categories.map.json"
+  },
+  {
+    "revision": "df49914e29eb97fc629aae79a996f5e6",
+    "url": "/data/prods.json"
+  },
+  {
+    "revision": "4b00206a4158e1095710bdbc5f15279a",
+    "url": "/data/profile.json"
+  },
+  {
+    "revision": "d273d63619c9aeaf15cdaf76422c4f87",
+    "url": "/fonts/Open_Sans/LICENSE.txt"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/fonts/Open_Sans/OpenSans-Bold.ttf"
+  },
+  {
+    "revision": "3a8113737b373d5bccd6f71d91408d16",
+    "url": "/fonts/Open_Sans/OpenSans-BoldItalic.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/fonts/Open_Sans/OpenSans-ExtraBold.ttf"
+  },
+  {
+    "revision": "37f52104364c2eb5482fd85777bda0ac",
+    "url": "/fonts/Open_Sans/OpenSans-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/fonts/Open_Sans/OpenSans-Light.ttf"
+  },
+  {
+    "revision": "c147d1302b974387afd38590072e7294",
+    "url": "/fonts/Open_Sans/OpenSans-LightItalic.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fonts/Open_Sans/OpenSans-Regular.ttf"
+  },
+  {
+    "revision": "f6238deb7f40a7a03134c11fb63ad387",
+    "url": "/fonts/Open_Sans/OpenSans-RegularItalic.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/fonts/Open_Sans/OpenSans-SemiBold.ttf"
+  },
+  {
+    "revision": "4f04fe541ca8be9b60b500e911b75fb5",
+    "url": "/fonts/Open_Sans/OpenSans-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "e3242149669bebf6afc4b0b5d22ec6ae",
+    "url": "/fonts/montserrat/Montserrat-Black.ttf"
+  },
+  {
+    "revision": "59a08c76185220c732ed4926d32a6706",
+    "url": "/fonts/montserrat/Montserrat-BlackItalic.ttf"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/fonts/montserrat/Montserrat-Bold.ttf"
+  },
+  {
+    "revision": "215a9e631fb43de893093e70db016cff",
+    "url": "/fonts/montserrat/Montserrat-BoldItalic.ttf"
+  },
+  {
+    "revision": "e375c6fe9bbeadb38d4911107e480378",
+    "url": "/fonts/montserrat/Montserrat-ExtraBold.ttf"
+  },
+  {
+    "revision": "9b9e2f4637ef72c726fda60e06b78d13",
+    "url": "/fonts/montserrat/Montserrat-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "94603128108f9cf9b26626564265c1d0",
+    "url": "/fonts/montserrat/Montserrat-ExtraLight.ttf"
+  },
+  {
+    "revision": "f9ab30a63e119d88351c7bd8d87a8459",
+    "url": "/fonts/montserrat/Montserrat-ExtraLightItalic.ttf"
+  },
+  {
+    "revision": "b0cf52e5f2f3e3fbc204cb06fd0e7b11",
+    "url": "/fonts/montserrat/Montserrat-Italic.ttf"
+  },
+  {
+    "revision": "2f354053d1c9c36f533ebd226698bfa5",
+    "url": "/fonts/montserrat/Montserrat-Light.ttf"
+  },
+  {
+    "revision": "3eb3e92074690a8bb2dc9f11a67d4a80",
+    "url": "/fonts/montserrat/Montserrat-LightItalic.ttf"
+  },
+  {
+    "revision": "0098f804fc2d06af52650e0b8ed3390c",
+    "url": "/fonts/montserrat/Montserrat-Medium.ttf"
+  },
+  {
+    "revision": "97573fc844f3f38e77c36bdc9ed07cbb",
+    "url": "/fonts/montserrat/Montserrat-MediumItalic.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/fonts/montserrat/Montserrat-Regular.ttf"
+  },
+  {
+    "revision": "bbcd5bbb5993808a92df01a5dfef77cd",
+    "url": "/fonts/montserrat/Montserrat-SemiBold.ttf"
+  },
+  {
+    "revision": "9b5089e2084e7a0a80e3ea8ad870a260",
+    "url": "/fonts/montserrat/Montserrat-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "b50f5872459e8e17eaffd5d57a2b2f8e",
+    "url": "/fonts/montserrat/Montserrat-Thin.ttf"
+  },
+  {
+    "revision": "5ba39d1f3eea1841e63a434e62e7f2d4",
+    "url": "/fonts/montserrat/Montserrat-ThinItalic.ttf"
+  },
+  {
+    "revision": "d641109f46a9231b7a92d6a114302a2b",
+    "url": "/fonts/raleway/Raleway-Black.ttf"
+  },
+  {
+    "revision": "0f629c963866f90e4ffa05550def8af3",
+    "url": "/fonts/raleway/Raleway-BlackItalic.ttf"
+  },
+  {
+    "revision": "f5c9c1aa2ac56e1f75b17c63c0a594bc",
+    "url": "/fonts/raleway/Raleway-Bold.ttf"
+  },
+  {
+    "revision": "02d3658bf349556ce8c3486c55bb703b",
+    "url": "/fonts/raleway/Raleway-BoldItalic.ttf"
+  },
+  {
+    "revision": "299ddfe9ee671626b0fe97cd94cfbb3b",
+    "url": "/fonts/raleway/Raleway-ExtraBold.ttf"
+  },
+  {
+    "revision": "8832d30f890da4120277ce31511440f3",
+    "url": "/fonts/raleway/Raleway-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "ebd5c800e24e108dd3aeacb28e16c595",
+    "url": "/fonts/raleway/Raleway-ExtraLight.ttf"
+  },
+  {
+    "revision": "91fc8a226846c0aafdf32f1158585bee",
+    "url": "/fonts/raleway/Raleway-ExtraLightItalic.ttf"
+  },
+  {
+    "revision": "b1f58e1bdc559a465620e1e48d08f460",
+    "url": "/fonts/raleway/Raleway-Italic.ttf"
+  },
+  {
+    "revision": "6b562d7ca359ccb3259f570bcceb4f70",
+    "url": "/fonts/raleway/Raleway-Light.ttf"
+  },
+  {
+    "revision": "fa415a9d96fefcef9dcd7c0f8938bdcb",
+    "url": "/fonts/raleway/Raleway-LightItalic.ttf"
+  },
+  {
+    "revision": "2e763f88811273e662c149e71ba9e984",
+    "url": "/fonts/raleway/Raleway-Medium.ttf"
+  },
+  {
+    "revision": "c966b8622dc3df06f4dcb637212cd92f",
+    "url": "/fonts/raleway/Raleway-MediumItalic.ttf"
+  },
+  {
+    "revision": "2d4cd8722065da2ac700199273325752",
+    "url": "/fonts/raleway/Raleway-Regular.ttf"
+  },
+  {
+    "revision": "e18d3880935810355cd07b95337c381d",
+    "url": "/fonts/raleway/Raleway-SemiBold.ttf"
+  },
+  {
+    "revision": "4bd987dee3441560d67deaf159d2feda",
+    "url": "/fonts/raleway/Raleway-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "ebd07bb4e01077b3736004a5305ce741",
+    "url": "/fonts/raleway/Raleway-Thin.ttf"
+  },
+  {
+    "revision": "f8708e6ab83f9e2e11c2bd0c0ecf93a6",
+    "url": "/fonts/raleway/Raleway-ThinItalic.ttf"
+  },
+  {
+    "revision": "0a8cf27a6a962843fd6702cfad4a5f2c",
+    "url": "/fonts/raleway/SIL Open Font License.txt"
+  },
+  {
+    "revision": "e22ebf032d3ad60ef9d4aebaabfa82e8",
+    "url": "/fonts/roboto/Roboto-Black.ttf"
+  },
+  {
+    "revision": "05cce86b801630ef864bb3c5f099e290",
+    "url": "/fonts/roboto/Roboto-BlackItalic.ttf"
+  },
+  {
+    "revision": "6332643400d59a0cc8bac831c3bf19b7",
+    "url": "/fonts/roboto/Roboto-Bold.ttf"
+  },
+  {
+    "revision": "30e69df2b0aedd4623660ccac2e98933",
+    "url": "/fonts/roboto/Roboto-BoldItalic.ttf"
+  },
+  {
+    "revision": "1b61f9daf09284e0f821152b317627f7",
+    "url": "/fonts/roboto/Roboto-Italic.ttf"
+  },
+  {
+    "revision": "9b4b0845efa011f46a370c31ce517d9f",
+    "url": "/fonts/roboto/Roboto-Light.ttf"
+  },
+  {
+    "revision": "88a693aecb50dce3bdb5b641a49aecf3",
+    "url": "/fonts/roboto/Roboto-LightItalic.ttf"
+  },
+  {
+    "revision": "9dda4e591e6d4bce5d4a8e2a195dca07",
+    "url": "/fonts/roboto/Roboto-Medium.ttf"
+  },
+  {
+    "revision": "f3447f991bcbdd067ae7322c0a65919e",
+    "url": "/fonts/roboto/Roboto-MediumItalic.ttf"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/fonts/roboto/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "bb90fb913c5e9124a8c969fa01402a5b",
+    "url": "/fonts/roboto/Roboto-Thin.ttf"
+  },
+  {
+    "revision": "08fda7a024f75f5f2ff53a2ccdae9496",
+    "url": "/fonts/roboto/Roboto-ThinItalic.ttf"
+  },
+  {
+    "revision": "b9cd09c0b5b394a9e66a24ccda87d9fd",
+    "url": "/fonts/sourcesanspro/sourcesanspro.css"
+  },
+  {
+    "revision": "173382e6d0e2d3eae90429dc42afa765",
+    "url": "/fonts/sourcesanspro/sourcesanspro.ttf"
+  },
+  {
+    "revision": "9bd66a0580cadf71bc3996792d7a0747",
+    "url": "/fonts/sourcesanspro/sourcesanspro.woff"
+  },
+  {
+    "revision": "681f5795aa0f988ec3ee5836fd0e1e6e",
+    "url": "/fonts/sourcesanspro/sourcesanspro.woff2"
+  },
+  {
+    "revision": "31a4c46adb34e30ca8b7868db898e2df",
+    "url": "/fonts/sourcesanspro/sourcesansproblack.ttf"
+  },
+  {
+    "revision": "add873a7d9ff6697732091aeed4bd2fc",
+    "url": "/fonts/sourcesanspro/sourcesansproblack.woff"
+  },
+  {
+    "revision": "6e1796be410081f2f93ac768473008aa",
+    "url": "/fonts/sourcesanspro/sourcesansproblack.woff2"
+  },
+  {
+    "revision": "7af3114d10abd77a6e990e4f466ec1ae",
+    "url": "/fonts/sourcesanspro/sourcesansproblackitalic.ttf"
+  },
+  {
+    "revision": "a3c012e5c4aa0397b68ba994614536f5",
+    "url": "/fonts/sourcesanspro/sourcesansproblackitalic.woff"
+  },
+  {
+    "revision": "010f7d73ab0f9a508b5bed8c5e211c43",
+    "url": "/fonts/sourcesanspro/sourcesansproblackitalic.woff2"
+  },
+  {
+    "revision": "5790837133f526465a3b12207eb50fc7",
+    "url": "/fonts/sourcesanspro/sourcesansprobold.ttf"
+  },
+  {
+    "revision": "faee4ee7734b629375d557fe6b28b1a9",
+    "url": "/fonts/sourcesanspro/sourcesansprobold.woff"
+  },
+  {
+    "revision": "bceacce118759047b6f188e89c4679c2",
+    "url": "/fonts/sourcesanspro/sourcesansprobold.woff2"
+  },
+  {
+    "revision": "c364f6016cfdc665db35f9a987ec4eae",
+    "url": "/fonts/sourcesanspro/sourcesansprobolditalic.ttf"
+  },
+  {
+    "revision": "32cc9abbaf26b3e95d8dce92e431f5c8",
+    "url": "/fonts/sourcesanspro/sourcesansprobolditalic.woff"
+  },
+  {
+    "revision": "2354f16cc4acbd43d09e9086367a26ae",
+    "url": "/fonts/sourcesanspro/sourcesansprobolditalic.woff2"
+  },
+  {
+    "revision": "d43fc4205d599ddbb59d6027fe8695d6",
+    "url": "/fonts/sourcesanspro/sourcesansproextralight.ttf"
+  },
+  {
+    "revision": "a0063366e4882e7cb13e25d0d1122ab6",
+    "url": "/fonts/sourcesanspro/sourcesansproextralight.woff"
+  },
+  {
+    "revision": "eedb685977eccd29e66918307e5d0039",
+    "url": "/fonts/sourcesanspro/sourcesansproextralight.woff2"
+  },
+  {
+    "revision": "ad3083af5f1b367f965862f4597c5279",
+    "url": "/fonts/sourcesanspro/sourcesansproextralightitalic.ttf"
+  },
+  {
+    "revision": "ccd35985cfa1d34372cfa24687249984",
+    "url": "/fonts/sourcesanspro/sourcesansproextralightitalic.woff"
+  },
+  {
+    "revision": "390f85ae2e9f63a0bc997e5c77871887",
+    "url": "/fonts/sourcesanspro/sourcesansproextralightitalic.woff2"
+  },
+  {
+    "revision": "2e9e09897dec5fb38e4a10b8741cab29",
+    "url": "/fonts/sourcesanspro/sourcesansproitalic.ttf"
+  },
+  {
+    "revision": "d2d359c5a8bcb58065b228c573b4f6d5",
+    "url": "/fonts/sourcesanspro/sourcesansproitalic.woff"
+  },
+  {
+    "revision": "df49992017d47ca1cdcbcbd54f4a6da9",
+    "url": "/fonts/sourcesanspro/sourcesansproitalic.woff2"
+  },
+  {
+    "revision": "4849a28135ded69d3be91edb5c8ea657",
+    "url": "/fonts/sourcesanspro/sourcesansprolight.ttf"
+  },
+  {
+    "revision": "bd382885ebf37ba4249d040a7289f615",
+    "url": "/fonts/sourcesanspro/sourcesansprolight.woff"
+  },
+  {
+    "revision": "de0cf4e120ad11f0bf15736acbb7e15e",
+    "url": "/fonts/sourcesanspro/sourcesansprolight.woff2"
+  },
+  {
+    "revision": "79d5772f271004f0873895ba0144052b",
+    "url": "/fonts/sourcesanspro/sourcesansprolightitalic.ttf"
+  },
+  {
+    "revision": "4b93cf66b179153f1ab0f07097096007",
+    "url": "/fonts/sourcesanspro/sourcesansprolightitalic.woff"
+  },
+  {
+    "revision": "9c3fcb0ef8c35bb7ad26c0ecdf82663e",
+    "url": "/fonts/sourcesanspro/sourcesansprolightitalic.woff2"
+  },
+  {
+    "revision": "17f8386492c03a964cc7af92913f53e9",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibold.ttf"
+  },
+  {
+    "revision": "e43fc02d666462ab9b0fc11edb30df13",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibold.woff"
+  },
+  {
+    "revision": "084fc1ae3a8dfa9097e7a532080836ea",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibold.woff2"
+  },
+  {
+    "revision": "92fefa88ef1672cba55113bfd0637c91",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibolditalic.ttf"
+  },
+  {
+    "revision": "6bb419eb16df8f808b011f80f8b4e6b0",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibolditalic.woff"
+  },
+  {
+    "revision": "1b4917080bf7511308c3917fe63d3236",
+    "url": "/fonts/sourcesanspro/sourcesansprosemibolditalic.woff2"
+  },
+  {
+    "revision": "5b1ddbe61ba4e4af878ba7f5ed1ab8be",
+    "url": "/img/arrow-down.svg"
+  },
+  {
+    "revision": "a0e061b54507c1ea6361ee20ac930f2c",
+    "url": "/img/arrow-right.svg"
+  },
+  {
+    "revision": "f96bc2f0abe2a578307d7cea59d64496",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "df132d79acb80a6e7247180c5afa1848",
+    "url": "/img/cart-2.svg"
+  },
+  {
+    "revision": "f61faa856d5338dc65a90a9275802a1e",
+    "url": "/img/cart-3.svg"
+  },
+  {
+    "revision": "9656e12e0833cf7f7bf267af9f28656a",
+    "url": "/img/cart-4.svg"
+  },
+  {
+    "revision": "c2601387e13fbd4deeca74e1bcee2ca8",
+    "url": "/img/cart-add.svg"
+  },
+  {
+    "revision": "9656e12e0833cf7f7bf267af9f28656a",
+    "url": "/img/cart-main.svg"
+  },
+  {
+    "revision": "2f79694de210f873ca01300355f8631f",
+    "url": "/img/cart.svg"
+  },
+  {
+    "revision": "c0cbd0847616c5b88e858a5e2abacdbe",
+    "url": "/img/check.svg"
+  },
+  {
+    "revision": "9eea5d017cd8e7aba5f305a0c151e0fd",
+    "url": "/img/close.svg"
+  },
+  {
+    "revision": "a088a01c361e25933eef66100aec3c50",
+    "url": "/img/flowers-3.jpg"
+  },
+  {
+    "revision": "45d8769cfaa38b127d7cc314ae16656a",
+    "url": "/img/mail.svg"
+  },
+  {
+    "revision": "de32c29b6e15432ce6d9fb3f0fcb1d56",
+    "url": "/img/map.jpg"
+  },
+  {
+    "revision": "9ec6ef164ec16634e8400e7325d65794",
+    "url": "/img/ok.svg"
+  },
+  {
+    "revision": "49a2db189979ccb89d9bd0544c14423e",
+    "url": "/img/user.svg"
+  },
+  {
+    "revision": "d08fe89e5837166dce54b255e5cf9032",
+    "url": "/img/vk.svg"
+  },
+  {
+    "revision": "3feaae0e4ae8197347a6d32407371d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b734297ea6ea7a2b4004",
+    "url": "/js/chunk-0bfdaa3a.63a5f2f4.js"
+  },
+  {
+    "revision": "dd41b87bb3b4df8587af",
+    "url": "/js/chunk-4475bce5.18c94c58.js"
+  },
+  {
+    "revision": "283c202e35b538cafd76",
+    "url": "/js/chunk-554e2c80.c92ccd96.js"
+  },
+  {
+    "revision": "6bd72c89ea4ebdd783e3",
+    "url": "/js/chunk-6ce6342e.1d9c4f97.js"
+  },
+  {
+    "revision": "6acd1593501ab07edd92",
+    "url": "/js/chunk-94988738.3e33d8c1.js"
+  },
+  {
+    "revision": "916b9b4b3b867bf8d478",
+    "url": "/js/chunk-b929a942.6429df3d.js"
+  },
+  {
+    "revision": "3947a7a60121259f7abb",
+    "url": "/js/chunk-fad08be4.f187bca9.js"
+  },
+  {
+    "revision": "f6244f47eb080989e9a5",
+    "url": "/js/chunk-fd2a7294.ac196642.js"
+  },
+  {
+    "revision": "863eea74da0c0c81e520",
+    "url": "/js/chunk-vendors.62b5eacc.js"
+  },
+  {
+    "revision": "965c234c49e7493d928b",
+    "url": "/js/index.564f3f05.js"
+  },
+  {
+    "revision": "882f04bb80a9d12eb94090078504ef2e",
+    "url": "/manifest.json"
+  }
+]);
